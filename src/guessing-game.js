5ced397f52d8ad782bd1127029a7a86938dae186
Ksenia Kuntsevich <kse.kuntsevich@icloud.com>
2019-10-07 14:34:43 +0300

    feat: add guessing game task solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,19 +1,28 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+    	this.min = 0;
+    	this.max = 0;
+    	this.guessNumber = 0;
+    }
 
     setRange(min, max) {
-
+    	this.min = min;
+    	this.max = max;
     }
 
     guess() {
+    	this.guessNumber = Math.round((this.max - this.min) / 2 + this.min);
+    	return this.guessNumber;
 
     }
 
     lower() {
+    	this.max = this.guessNumber;
 
     }
 
     greater() {
+    	this.min = this.guessNumber;
 
     }
 }
